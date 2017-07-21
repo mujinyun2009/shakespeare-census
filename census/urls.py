3151fd0fb2400609a218993b18349070fbce6a61
Roshan Benefo <rbenefoz@gmail.com>
2017-07-21 14:53:31 -0400

    add password reset capability

--- census/urls.py
+++ census/urls.py
@@ -55,18 +55,18 @@ urlpatterns = [
     url(r'^password_reset/$', password_reset,
         {'template_name': 'census/password_reset_form.html',
          'email_template_name': 'census/password_reset_email.html',
-         'subject_template_name': 'WSKsearch/password_reset_subject.txt'},
+         'subject_template_name': 'census/password_reset_subject.txt'},
         name='password_reset'),
     url(r'^user/password_reset_done/$', password_reset_done,
-        {'template_name': 'WSKsearch/password_reset_done.html'},
+        {'template_name': 'census/password_reset_done.html'},
         name='password_reset_done'),
     url(r'^user/password_reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
         '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         password_reset_confirm,
-        {'template_name': 'WSKsearch/password_reset_confirm.html'},
+        {'template_name': 'census/password_reset_confirm.html'},
         name='password_reset_confirm'),
     url(r'^user/password_reset/complete/$', password_reset_complete,
-        {'template_name': 'WSKsearch/password_reset_complete.html'},
+        {'template_name': 'census/password_reset_complete.html'},
         name='password_reset_complete'),
 
 	]
